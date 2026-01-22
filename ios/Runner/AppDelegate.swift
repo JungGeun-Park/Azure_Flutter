42d55f217a5c5d6d8851125e0025d7b92b164ec2
puzznic <puzznic@inka.co.kr>
2026-01-22 16:43:52 +0900

    d4

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -17,22 +17,22 @@ import UIKit
         if ( tamper > 0 )
         {
             var msg = "Abnormal Environment Detected !!";
-            if ( tamper & DETECTED_JAILBROKEN ) > 0
-                { msg += "\n - Jailbroken"; }
-            if ( tamper & DETECTED_DRM_DECRYPTED ) > 0
-                { msg += "\n - Executable is not encrypted"; }
-            if ( tamper & DETECTED_DEBUG_ATTACHED ) > 0
-                { msg += "\n - App is debugged"; }
-            if ( tamper & ( DETECTED_HASH_INFO_CORRUPTED | DETECTED_HASH_MODIFIED )) > 0
-                { msg += "\n - App integrity corrupted"; }
-            if ( tamper & ( DETECTED_CODESIGN_CORRUPTED | DETECTED_EXECUTABLE_CORRUPTED )) > 0
-                { msg += "\n - App executable has corrupted"; }
-            if ( tamper & DETECTED_CERTIFICATE_CHANGED ) > 0
-                { msg += "\n - App has re-signed"; }
-            if ( tamper & DETECTED_BLACKLIST_CORRUPTED ) > 0
-                { msg += "\n - Blacklist/Whitelist has corrupted or missing"; }
-            if ( tamper & DETECTED_CHEAT_TOOL ) > 0
-                { msg += "\n - Cheat tool has detected"; }
+//            if ( tamper & DETECTED_JAILBROKEN ) > 0
+//                { msg += "\n - Jailbroken"; }
+//            if ( tamper & DETECTED_DRM_DECRYPTED ) > 0
+//                { msg += "\n - Executable is not encrypted"; }
+//            if ( tamper & DETECTED_DEBUG_ATTACHED ) > 0
+//                { msg += "\n - App is debugged"; }
+//            if ( tamper & ( DETECTED_HASH_INFO_CORRUPTED | DETECTED_HASH_MODIFIED )) > 0
+//                { msg += "\n - App integrity corrupted"; }
+//            if ( tamper & ( DETECTED_CODESIGN_CORRUPTED | DETECTED_EXECUTABLE_CORRUPTED )) > 0
+//                { msg += "\n - App executable has corrupted"; }
+//            if ( tamper & DETECTED_CERTIFICATE_CHANGED ) > 0
+//                { msg += "\n - App has re-signed"; }
+//            if ( tamper & DETECTED_BLACKLIST_CORRUPTED ) > 0
+//                { msg += "\n - Blacklist/Whitelist has corrupted or missing"; }
+//            if ( tamper & DETECTED_CHEAT_TOOL ) > 0
+//                { msg += "\n - Cheat tool has detected"; }
 
             let alertController = UIAlertController(title: "AppSealing", message: msg, preferredStyle: .alert );
             alertController.addAction(UIAlertAction(title: "Confirm", style: .default,
